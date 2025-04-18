46d703dd3bc154d6df1bdfdcfd1c1f0c00d81a71
wesselvandalen <wesselvandalen@gmail.com>
2025-04-18 23:35:16 +0200

    Bygget Home komponenten

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,11 +2,19 @@ import './home.css';
 import hytte from '../assets/hytte.png';
 
 export default function Home() {
+
     return (
         <div className="home-container">
             <img src={hytte} alt="Bakgrunnsbilde av en skog." className='background-image' />
             <div className="home-content">
-                
+                <h3 className='home-title'>Hytte Leie – Finn din perfekte hytte i naturen</h3>
+                <p className='home-description'>Lei en koselig hytte i vakre omgivelser, fra skog til fjell. Enkel booking og unike opplevelser for ro og eventyr – alt på ett sted.</p>
+                <input
+                    type="text"
+                    id='searchbar'
+                    className='searchbar'
+                    placeholder={`Finn din hytte her`}
+                />
             </div>
         </div>
     );
